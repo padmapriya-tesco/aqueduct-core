2d64d34202509a6876e87935bb57becf0b58c397
Adam Yorwerth <Adam.Yorwerth@uk.tesco.com>
2019-08-21 11:19:51 +0100

    Added back constructor

--- pipe-api/src/main/java/com/tesco/aqueduct/pipe/api/PipeStateResponse.java
+++ pipe-api/src/main/java/com/tesco/aqueduct/pipe/api/PipeStateResponse.java
@@ -1,9 +1,18 @@
 package com.tesco.aqueduct.pipe.api;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class PipeStateResponse {
+    @JsonProperty
     private boolean isUpToDate;
+
+    @JsonProperty
     private long localOffset;
+
+    public PipeStateResponse(final boolean isUpToDate, final long localOffset) {
+        this.isUpToDate = isUpToDate;
+        this.localOffset = localOffset;
+    }
 }
