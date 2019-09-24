f5504d344551b141af792294097edbfb9a1f6513
Ben Noble <ben.noble@tesco.com>
2019-09-24 15:11:07 +0100

    Added a top-level status controller. Meant renaming the pipe status controller

---
+++ pipe-http-server/src/main/java/com/tesco/aqueduct/pipe/http/PipeStatusController.java
@@ -0,0 +1,25 @@
+package com.tesco.aqueduct.pipe.http;
+
+import com.tesco.aqueduct.pipe.metrics.Measure;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.security.annotation.Secured;
+import io.micronaut.security.rules.SecurityRule;
+import lombok.Data;
+
+@Secured(SecurityRule.IS_ANONYMOUS)
+@Controller
+@Measure
+public class PipeStatusController {
+
+    @Get("/pipe/_status")
+    Status status() {
+        return new Status("ok", Version.getImplementationVersion());
+    }
+
+    @Data
+    private static class Status {
+        private final String status;
+        private final String version;
+    }
+}
---
+++ pipe-http-server/src/test/groovy/com/tesco/aqueduct/pipe/http/PipeStatusControllerSpec.groovy
@@ -0,0 +1,45 @@
+package com.tesco.aqueduct.pipe.http
+
+import io.micronaut.context.ApplicationContext
+import io.micronaut.http.HttpStatus
+import io.micronaut.runtime.server.EmbeddedServer
+import io.restassured.RestAssured
+import spock.lang.AutoCleanup
+import spock.lang.Shared
+import spock.lang.Specification
+
+import static io.restassured.RestAssured.when
+import static org.hamcrest.Matchers.equalTo
+
+class PipeStatusControllerSpec extends Specification {
+
+    @Shared @AutoCleanup("stop") ApplicationContext context
+    @Shared @AutoCleanup("stop") EmbeddedServer server
+
+    void setupSpec() {
+        context = ApplicationContext
+            .build()
+            .mainClass(PipeStatusController)
+            .build()
+
+        context.start()
+
+        server = context.getBean(EmbeddedServer)
+        server.start()
+
+        RestAssured.port = server.port
+    }
+
+    void cleanupSpec() {
+        RestAssured.port = RestAssured.DEFAULT_PORT
+    }
+
+    def "pipe status contains version in it"() {
+        expect:
+        when()
+            .get("/pipe/_status")
+            .then()
+            .statusCode(HttpStatus.OK.code)
+            .body("version", equalTo(Version.getImplementationVersion()))
+    }
+}
