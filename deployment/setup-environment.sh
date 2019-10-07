8dab7909b39fc0dc4986c852a96377dacf395e7b
Christopher <cwebbtw@Christophers-MacBook-Pro.local>
2019-10-07 14:31:57 +0100

    CW Removing superfluous deployment files

--- deployment/setup-environment.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-kubectl apply -f k8s/aqueduct-pipe-namespace.yaml
-kubectl apply -f k8s/aqueduct-pipe-deployer-account.yaml
-kubectl apply -f k8s/aqueduct-pipe-service.yaml
