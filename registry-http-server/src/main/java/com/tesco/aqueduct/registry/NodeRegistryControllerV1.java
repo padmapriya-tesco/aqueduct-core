8b73d9803b0148fcefec5a44413618cf83a2f92f
Ben Noble <ben.noble@tesco.com>
2019-10-08 14:22:20 +0100

    Added V1 urls for registry

---
+++ registry-http-server/src/main/java/com/tesco/aqueduct/registry/NodeRegistryControllerV1.java
@@ -0,0 +1,63 @@
+package com.tesco.aqueduct.registry;
+
+import com.tesco.aqueduct.pipe.api.MessageReader;
+import com.tesco.aqueduct.pipe.metrics.Measure;
+import com.tesco.aqueduct.registry.model.Node;
+import com.tesco.aqueduct.registry.model.StateSummary;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.annotation.*;
+import io.micronaut.security.annotation.Secured;
+import io.micronaut.security.rules.SecurityRule;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.Nullable;
+import javax.inject.Inject;
+import java.net.URL;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Measure
+@Controller("/v1/registry")
+public class NodeRegistryControllerV1 {
+    private static final String REGISTRY_DELETE = "REGISTRY_DELETE";
+
+    private static final RegistryLogger LOG = new RegistryLogger(LoggerFactory.getLogger(NodeRegistryControllerV1.class));
+
+    @Inject
+    private NodeRegistry registry;
+
+    // This is temporary, it might be better for us to make pipe depend on registry and have it register itself in it.
+    @Inject
+    private MessageReader pipe;
+
+    public NodeRegistryControllerV1(final NodeRegistry registry) {
+        this.registry = registry;
+    }
+
+    @Secured(SecurityRule.IS_ANONYMOUS)
+    @Get
+    public StateSummary getSummary(@Nullable final List<String> groups) {
+        return registry.getSummary(pipe.getLatestOffsetMatching(null), "ok", groups);
+    }
+
+    @Secured(SecurityRule.IS_AUTHENTICATED)
+    @Post
+    public List<URL> registerNode(@Body final Node node) {
+        final List<URL> requestedToFollow = registry.register(node);
+        final String followStr = requestedToFollow.stream().map(URL::toString).collect(Collectors.joining(","));
+        LOG.withNode(node).info("requested to follow", followStr);
+        return requestedToFollow;
+    }
+
+    @Secured(REGISTRY_DELETE)
+    @Delete("/{group}/{id}")
+    public HttpResponse deleteNode(final String group, final String id) {
+        final boolean deleted = registry.deleteNode(group, id);
+        if (deleted) {
+            return HttpResponse.status(HttpStatus.OK);
+        } else {
+            return HttpResponse.status(HttpStatus.NOT_FOUND);
+        }
+    }
+}
