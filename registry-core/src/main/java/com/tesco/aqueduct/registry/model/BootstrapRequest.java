e462c7dfcfc06b6815125e9b993d2f7ce3823c59
sebastianlavender <sebastianlavender@outlook.com>
2019-10-10 19:28:37 +0100

    SL removed unneeded Lombok from BootstrapRequest

--- registry-core/src/main/java/com/tesco/aqueduct/registry/model/BootstrapRequest.java
+++ registry-core/src/main/java/com/tesco/aqueduct/registry/model/BootstrapRequest.java
@@ -1,12 +1,10 @@
 package com.tesco.aqueduct.registry.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.Builder;
 import lombok.Data;
 
 import java.util.List;
 
-@Builder(toBuilder = true)
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class BootstrapRequest {
