b72f9c6061bc987bc501e96fc51082ed7044144d
sebastianlavender <sebastianlavender@outlook.com>
2019-11-11 16:14:22 +0000

    SL: -added identity variables in

--- pipe-http-server-cloud/src/main/java/com/tesco/aqueduct/pipe/identity/IdentityTokenValidatorClient.java
+++ pipe-http-server-cloud/src/main/java/com/tesco/aqueduct/pipe/identity/IdentityTokenValidatorClient.java
@@ -6,10 +6,10 @@ import io.micronaut.http.annotation.Post;
 import io.micronaut.http.client.annotation.Client;
 import io.reactivex.Flowable;
 
-@Client("${identity.url}")
+@Client("identity.url")
 public interface IdentityTokenValidatorClient {
 
-    @Post("${identity.validate.token.path}")
+    @Post("identity.validate.token.path")
     Flowable<ValidateTokenResponse> validateToken(
         @Header("TraceId") String traceId,
         @Body ValidateTokenRequest identityRequest
