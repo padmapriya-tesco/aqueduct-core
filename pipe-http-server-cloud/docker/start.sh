812e54da4ed29845195ec7e5942f0e3a3a4becea
sebastianlavender <sebastianlavender@outlook.com>
2019-11-21 13:47:42 +0000

    SL: - moved more start.sh variables out to environment variables

--- pipe-http-server-cloud/docker/start.sh
+++ pipe-http-server-cloud/docker/start.sh
@@ -1,18 +1,12 @@
 #!/usr/bin/env bash
 
 pipe_read_username=$(printf "%q" ${PIPE_READ_USERNAME:-$1})
-pipe_read_password=$(printf "%q" ${PIPE_READ_PASSWORD:-$2})
-runscope_pipe_read_username=$(printf "%q" ${RUNSCOPE_PIPE_READ_USERNAME:-$3})
-runscope_pipe_read_password=$(printf "%q" ${RUNSCOPE_PIPE_READ_PASSWORD:-${4}})
-support_username=$(printf "%q" ${SUPPORT_USERNAME:-${5}})
-support_password=$(printf "%q" ${SUPPORT_PASSWORD:-${6}})
+runscope_pipe_read_username=$(printf "%q" ${RUNSCOPE_PIPE_READ_USERNAME:-$2})
+support_username=$(printf "%q" ${SUPPORT_USERNAME:-${3}})
 
 sed -i "s/{PIPE_READ_USERNAME}/$pipe_read_username/" /etc/aqueduct/pipe/application.yml
-sed -i "s/{PIPE_READ_PASSWORD}/$pipe_read_password/" /etc/aqueduct/pipe/application.yml
 sed -i "s/{RUNSCOPE_PIPE_READ_USERNAME}/$runscope_pipe_read_username/" /etc/aqueduct/pipe/application.yml
-sed -i "s/{RUNSCOPE_PIPE_READ_PASSWORD}/$runscope_pipe_read_password/" /etc/aqueduct/pipe/application.yml
 sed -i "s/{SUPPORT_USERNAME}/$support_username/" /etc/aqueduct/pipe/application.yml
-sed -i "s/{SUPPORT_PASSWORD}/$support_password/" /etc/aqueduct/pipe/application.yml
 
 exec java \
     -Dmicronaut.config.files=/etc/aqueduct/pipe/application.yml \
