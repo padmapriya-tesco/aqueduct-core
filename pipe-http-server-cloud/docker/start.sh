65d02eefab3b0a1f367eb249a44ab0678af5046c
Ben Noble <ben.noble@tesco.com>
2019-11-29 10:15:35 +0000

    Adding env specific config and removing variables from start.sh

--- pipe-http-server-cloud/docker/start.sh
+++ pipe-http-server-cloud/docker/start.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-pipe_read_username=$(printf "%q" ${PIPE_READ_USERNAME:-$7})
-runscope_pipe_read_username=$(printf "%q" ${RUNSCOPE_PIPE_READ_USERNAME:-$9})
-support_username=$(printf "%q" ${SUPPORT_USERNAME:-${11}})
-
-sed -i "s/{PIPE_READ_USERNAME}/$pipe_read_username/" /etc/aqueduct/pipe/application.yml
-sed -i "s/{RUNSCOPE_PIPE_READ_USERNAME}/$runscope_pipe_read_username/" /etc/aqueduct/pipe/application.yml
-sed -i "s/{SUPPORT_USERNAME}/$support_username/" /etc/aqueduct/pipe/application.yml
-
 exec java \
     -Dmicronaut.config.files=/etc/aqueduct/pipe/application.yml \
     -jar $(ls -1 /opt/aqueduct/pipe/pipe-http-server-cloud-*all.jar | tail -n 1)
