9514f9641f8ac46a9a80b164a64ccd7cc94877f7
Ben Noble <ben.noble@tesco.com>
2019-11-29 10:58:25 +0000

    Debug logging number of users

--- pipe-http-server/src/main/java/com/tesco/aqueduct/pipe/http/PipeReadAuthenticationProvider.java
+++ pipe-http-server/src/main/java/com/tesco/aqueduct/pipe/http/PipeReadAuthenticationProvider.java
@@ -1,9 +1,11 @@
 package com.tesco.aqueduct.pipe.http;
 
+import com.tesco.aqueduct.pipe.logger.PipeLogger;
 import io.micronaut.context.annotation.Requires;
 import io.micronaut.security.authentication.*;
 import io.reactivex.Flowable;
 import org.reactivestreams.Publisher;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -12,7 +14,7 @@ import java.util.List;
 @Singleton
 @Requires(property = "authentication.users")
 public class PipeReadAuthenticationProvider implements AuthenticationProvider {
-
+    private static final PipeLogger LOG = new PipeLogger(LoggerFactory.getLogger(PipeReadAuthenticationProvider.class));
     private final List<User> users;
 
     @Inject
@@ -29,7 +31,9 @@ public class PipeReadAuthenticationProvider implements AuthenticationProvider {
         );
     }
 
-    AuthenticationResponse authenticate(final Object username, final Object password) {
+    private AuthenticationResponse authenticate(final Object username, final Object password) {
+        int userCount = users.isEmpty() ? 0 : users.size();
+        LOG.debug("authenticate", "Users: " + userCount);
         return users.stream()
             .filter(user -> user.isAuthenticated(username, password))
             .findAny()
