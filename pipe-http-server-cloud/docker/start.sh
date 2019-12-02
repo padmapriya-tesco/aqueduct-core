d090fcb3e3e64256fb1a93364e25e031092e3058
Ben Noble <ben.noble@tesco.com>
2019-12-02 15:11:10 +0000

    Delete start.sh

--- pipe-http-server-cloud/docker/start.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-exec java \
-    -Dmicronaut.config.files=/etc/aqueduct/pipe/application.yml \
-    -jar $(ls -1 /opt/aqueduct/pipe/pipe-http-server-cloud-*all.jar | tail -n 1)
