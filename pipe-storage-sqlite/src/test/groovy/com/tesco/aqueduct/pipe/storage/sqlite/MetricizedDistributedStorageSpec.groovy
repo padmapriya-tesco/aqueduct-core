4d51b0a7623ed9724b314bf8cef91dc1904ff5d6
Lorenzo Cianciaruso <lorenzo.cianciaruso@tesco.com>
2020-02-27 10:55:22 +0000

    LC/AS #380 remove unused imports

--- pipe-storage-sqlite/src/test/groovy/com/tesco/aqueduct/pipe/storage/sqlite/MetricizedDistributedStorageSpec.groovy
+++ pipe-storage-sqlite/src/test/groovy/com/tesco/aqueduct/pipe/storage/sqlite/MetricizedDistributedStorageSpec.groovy
@@ -1,15 +1,11 @@
 package com.tesco.aqueduct.pipe.storage.sqlite
 
-import com.tesco.aqueduct.pipe.api.Message
+
 import com.tesco.aqueduct.pipe.api.OffsetEntity
 import com.tesco.aqueduct.pipe.api.OffsetName
-import io.micrometer.core.instrument.MeterRegistry
 import io.micrometer.core.instrument.simple.SimpleMeterRegistry
 import spock.lang.Specification
 import spock.lang.Unroll
-
-import java.time.ZonedDateTime
-import java.util.concurrent.atomic.AtomicLong
 
 class MetricizedDistributedStorageSpec extends Specification {
 
@@ -35,17 +31,5 @@ class MetricizedDistributedStorageSpec extends Specification {
         "pipe.offset.globalLatestOffset"    | OffsetName.GLOBAL_LATEST_OFFSET   | 100L
         "pipe.offset.localLatestOffset"     | OffsetName.LOCAL_LATEST_OFFSET    | 15L
         "pipe.offset.pipeOffset"            | OffsetName.PIPE_OFFSET            | 54465L
-    }
-
-    def "write message invoked timed distributed storage"() {
-        given: "a metricized distributed storage and an offset entity"
-        metricizedDistributedStorage = new MetricizedDistributedStorage(timedDistributedStorage, meterRegistry)
-        def message = new Message("type", "key", "content", 0, ZonedDateTime.now(), "data")
-
-        when: "an offset is written"
-        metricizedDistributedStorage.write(message)
-
-        then: "the offset metric is recorded and the timed distributed storage is invoked"
-        1 * timedDistributedStorage.write(message)
     }
 }
