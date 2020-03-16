ea2cc1b8814ac82d2c8e8816220ef5eea71d38a3
Lorenzo Cianciaruso <lorenzo.cianciaruso@tesco.com>
2020-03-16 14:13:32 +0000

    LC #131 Remove tests that are not relevant anymore

--- pipe-http-server-cloud/src/test/groovy/com/tesco/aqueduct/pipe/location/CloudLocationResolverSpec.groovy
+++ pipe-http-server-cloud/src/test/groovy/com/tesco/aqueduct/pipe/location/CloudLocationResolverSpec.groovy
@@ -22,29 +22,4 @@ class CloudLocationResolverSpec extends Specification {
         and: "target clusters are returned"
         locations == [new Cluster("cluster_A"), new Cluster("cluster_B")]
     }
-
-    def "Illegal argument error when location uuid passed is null"() {
-        given:
-        def locationUuid = null
-
-        and:
-        def locationServiceClient = Mock(LocationServiceClient)
-
-        when:
-        new CloudLocationResolver(locationServiceClient).resolve(locationUuid)
-
-        then:
-        thrown(IllegalArgumentException)
-    }
-
-    def "Illegal argument error when location client is null"() {
-        given:
-        def locationServiceClient = null
-
-        when:
-        new CloudLocationResolver(locationServiceClient)
-
-        then:
-        thrown(IllegalArgumentException)
-    }
 }
