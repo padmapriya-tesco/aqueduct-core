10633a71f7772e32d8cb7da06ad4405596b6c367
Ashish Sharma <ashish.sharma2@tesco.com>
2020-03-25 14:02:23 +0000

    #131 LC/AS: - Added error handling for 5xx status code from Identity issue token service.

---
+++ pipe-http-server-cloud/src/main/java/com/tesco/aqueduct/pipe/identity/issuer/IdentityServiceUnavailableException.java
@@ -0,0 +1,7 @@
+package com.tesco.aqueduct.pipe.identity.issuer;
+
+public class IdentityServiceUnavailableException extends RuntimeException {
+    public IdentityServiceUnavailableException(String message) {
+        super(message);
+    }
+}
--- pipe-http-server-cloud/src/main/java/com/tesco/aqueduct/pipe/location/CloudLocationResolver.java
+++ pipe-http-server-cloud/src/main/java/com/tesco/aqueduct/pipe/location/CloudLocationResolver.java
@@ -2,13 +2,17 @@ package com.tesco.aqueduct.pipe.location;
 
 import com.tesco.aqueduct.pipe.api.Cluster;
 import com.tesco.aqueduct.pipe.api.LocationResolver;
+import com.tesco.aqueduct.pipe.logger.PipeLogger;
 import io.micronaut.http.client.exceptions.HttpClientResponseException;
+import org.slf4j.LoggerFactory;
 
 import javax.validation.constraints.NotNull;
 import java.util.List;
 import java.util.UUID;
 
 public class CloudLocationResolver implements LocationResolver {
+
+    private static final PipeLogger LOG = new PipeLogger(LoggerFactory.getLogger(CloudLocationResolver.class));
 
     private final LocationServiceClient locationServiceClient;
 
@@ -18,11 +22,13 @@ public class CloudLocationResolver implements LocationResolver {
 
     @Override
     public List<Cluster> resolve(@NotNull String locationId) {
+        final String traceId = UUID.randomUUID().toString();
         try {
-            return locationServiceClient.getClusters(UUID.randomUUID().toString(), locationId).getClusters();
-        } catch(final HttpClientResponseException exception) {
+            return locationServiceClient.getClusters(traceId, locationId).getClusters();
+        } catch (final HttpClientResponseException exception) {
+            LOG.error("resolve", "trace_id: " + traceId, exception);
             if (exception.getStatus().getCode() > 499) {
-                throw new LocationServiceUnavailableException("Unexpected error from location service");
+                throw new LocationServiceUnavailableException("Unexpected error from location service with status - " + exception.getStatus());
             } else {
                 throw exception;
             }
