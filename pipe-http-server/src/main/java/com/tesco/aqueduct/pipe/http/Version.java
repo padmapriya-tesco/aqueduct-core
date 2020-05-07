262034bb5b7b4c06dd2fadfd17a37659776b5379
Ashish Sharma <ashish.sharma2@tesco.com>
2020-05-07 10:48:15 +0100

    #407 LC/AS: - Added manifest with implementation version

--- pipe-http-server/src/main/java/com/tesco/aqueduct/pipe/http/Version.java
+++ pipe-http-server/src/main/java/com/tesco/aqueduct/pipe/http/Version.java
@@ -4,6 +4,10 @@ public class Version {
 
     private static final String DEFAULT_VERSION = "0.0.0";
 
+    public static void main(String[] args) {
+        System.out.println(Version.getImplementationVersion());
+    }
+
     public static String getImplementationVersion() {
         final String version = Version.class.getPackage().getImplementationVersion();
         return version == null ? DEFAULT_VERSION : version;
