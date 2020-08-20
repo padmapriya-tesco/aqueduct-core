8c6f891edf1d601e24db71dc2ced6756d2d0684e
Ashish Sharma <ashish.sharma2@tesco.com>
2020-08-20 18:13:34 +0100

    #264: AS/SL Small comment refactoring

--- pipe-http-server/src/main/java/com/tesco/aqueduct/pipe/http/HttpNoFrameworkCompressionStrategy.java
+++ pipe-http-server/src/main/java/com/tesco/aqueduct/pipe/http/HttpNoFrameworkCompressionStrategy.java
@@ -4,9 +4,9 @@ import io.micronaut.http.server.netty.HttpCompressionStrategy;
 import io.netty.handler.codec.http.HttpResponse;
 
 /**
- * Compression strategy hook provided by Micronaut to enable/disable smart gzip compression it performs by default.
+ * Compression strategy hook provided by Micronaut to disable smart gzip compression it performs by default.
  * Micronaut has a content size threshold where it compresses by default if content size is above certain threshold,
- * another way to disable smart compression by Micronaut is to specify large threshold within configuration.
+ * another way to disable smart compression is to specify large threshold value within configuration.
  */
 public class HttpNoFrameworkCompressionStrategy implements HttpCompressionStrategy {
     @Override
