f46b2144e1a1e26c889058759202b293147a713c
Lorenzo Cianciaruso <lorenzo.cianciaruso@tesco.com>
2020-09-21 14:09:47 +0100

    LC/SL add support for gzip internal client

---
+++ pipe-http-client/src/main/java/com/tesco/aqueduct/pipe/http/client/InternalBrotliHttpPipeClient.java
@@ -0,0 +1,25 @@
+package com.tesco.aqueduct.pipe.http.client;
+
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.annotation.Consumes;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Header;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.retry.annotation.CircuitBreaker;
+
+import javax.annotation.Nullable;
+import java.util.List;
+
+@Client(id = "pipe")
+public interface InternalBrotliHttpPipeClient extends InternalHttpPipeClient {
+
+    @Get("/pipe/{offset}{?type,location}")
+    @Consumes
+    @Header(name="Accept-Encoding", value="br")
+    @CircuitBreaker(delay = "${pipe.http.client.delay}", attempts = "${pipe.http.client.attempts}", reset = "${pipe.http.client.reset}")
+    HttpResponse<byte[]> httpRead(
+        @Nullable List<String> type,
+        long offset,
+        String location
+    );
+}
