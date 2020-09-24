5851b4e674a4f6f71a1486e9580b525a05470c35
hih9 <adam.yorwerth@tesco.com>
2020-09-24 13:24:05 +0100

    integration test fix

--- pipe-http-server-cloud/src/integration/groovy/com/tesco/aqueduct/pipe/CodecIntegrationSpec.groovy
+++ pipe-http-server-cloud/src/integration/groovy/com/tesco/aqueduct/pipe/CodecIntegrationSpec.groovy
@@ -68,7 +68,8 @@ class CodecIntegrationSpec extends Specification {
                 "location.attempts":                            3,
                 "location.delay":                               "10ms",
 
-                "compression.threshold-in-bytes":               1024
+                "compression.threshold-in-bytes":               1024,
+                "compression.logging":                          false
             )
             .mainClass(EmbeddedServer)
             .build()
