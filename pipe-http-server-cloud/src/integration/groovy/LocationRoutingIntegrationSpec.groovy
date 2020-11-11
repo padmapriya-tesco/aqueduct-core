af6324c76d9baf14eda9ec44721e6b326e8a6d55
Lorenzo Cianciaruso <lorenzo.cianciaruso@tesco.com>
2020-11-11 14:48:52 +0000

    LC fic integration test

--- pipe-http-server-cloud/src/integration/groovy/LocationRoutingIntegrationSpec.groovy
+++ pipe-http-server-cloud/src/integration/groovy/LocationRoutingIntegrationSpec.groovy
@@ -66,6 +66,7 @@ class LocationRoutingIntegrationSpec extends Specification {
                 "persistence.read.max-batch-size":              "10485760",
                 "persistence.read.expected-node-count":         2,
                 "persistence.read.cluster-db-pool-size":        10,
+                "persistence.read.read-delay-seconds":          0,
 
                 "authentication.identity.url":                  "${identityMockService.getHttpUrl()}",
                 "authentication.identity.validate.token.path":  "$VALIDATE_TOKEN_PATH",
