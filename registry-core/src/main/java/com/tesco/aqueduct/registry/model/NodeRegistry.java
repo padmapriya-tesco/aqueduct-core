13d6b077f93469cbdf273474992af9a4d4bdb793
Lorenzo Cianciaruso <lorenzo.cianciaruso@tesco.com>
2021-02-01 12:37:40 +0100

    LC/AY Add comment as for PR comments

--- registry-core/src/main/java/com/tesco/aqueduct/registry/model/NodeRegistry.java
+++ registry-core/src/main/java/com/tesco/aqueduct/registry/model/NodeRegistry.java
@@ -3,9 +3,18 @@ package com.tesco.aqueduct.registry.model;
 import java.util.List;
 
 public interface NodeRegistry {
-
+    /**
+     * @param node Node to register as new or update current state
+     * @return Node registered
+     */
     Node register(Node node);
 
+    /**
+     * @param offset Latest offset of root
+     * @param status Status of root
+     * @param groups List of groups to return, all if empty or null
+     * @return Summary of all currently known nodes
+     */
     StateSummary getSummary(long offset, Status status, List<String> groups);
 
     boolean deleteNode(String group, String host);
